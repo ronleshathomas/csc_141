31c8e9a950a8576d6dda27fd3e8d27c6ee5e243d
ronleshathomas <ronleshathomas24@gmail.com>
2025-09-04 12:32:41 -0400

    Add files via upload

---
+++ assignments/01_getting_started/01_03_infinite_skills.py
@@ -0,0 +1,7 @@
+"""
+For my programs I want to make something fun, something colorful, and something different.
+So my something fun would be a game, similar to crossy road.
+My something colorful would be a drawing program, where you can draw and colorin different pictures.
+My something different would be a music program, where you can make your own music, by using the keyboard.
+These are ambitious but I hope to understand python by the end of this. 
+"""
