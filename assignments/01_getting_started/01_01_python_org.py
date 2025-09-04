a019a1dfc53de70a8ab66f3f5f3184d905bf518a
ronleshathomas <ronleshathomas24@gmail.com>
2025-09-04 12:21:51 -0400

    Add files via upload

---
+++ assignments/01_getting_started/01_01_python_org.py
@@ -0,0 +1,8 @@
+"""
+In python.org I looked through the FAQ since I am new to python.
+In terms of learning python, I found the genral section to be guide for a beginner like me, it was very helpful.
+So pyhton is a free programming language that is supposed to be easy to learn and has a clear syntax.
+Looking at its successes I saw that python is used by many big companies like Google, Facebook, Instagram, and many more.
+It was also used in a drug discovery project which is very interesting.
+Overall python seems to be an extensively known and used programming language,that is new to me.
+"""
