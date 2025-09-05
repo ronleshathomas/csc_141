88eace393db86f4d559edfaf52e444bc894cdaa4
ronleshathomas <ronleshathomas24@gmail.com>
2025-09-05 11:54:55 -0400

    Add files via upload

---
+++ assignments/02_variables_and_data_types/02_11_adding_comments.py
@@ -0,0 +1,7 @@
+# ron thomas, 9/4/25, this is one of my first python programs.
+message= "hello, world" 
+print(message)
+
+# ron thomas, 9/4/25, this is the last song I listened to. 
+print ("Jhene Aiko once said,'but your the same stranger'")
+
