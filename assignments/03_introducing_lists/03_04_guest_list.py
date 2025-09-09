fff4a05bc131800e593ede5d110b6f8df0ae8957
ronleshathomas <ronleshathomas24@gmail.com>
2025-09-09 12:35:25 -0400

    Add files via upload

---
+++ assignments/03_introducing_lists/03_04_guest_list.py
@@ -0,0 +1,7 @@
+guest = ['michelle obama', 'beyonce', 'marsai martin']
+message = f"I would like to formally invite you to dinner Ms,{guest[0].title()}."
+print(message)
+message = f"You are my idol and I would love if you came to dinner with me Ms.{guest[1].title()}."
+print(message)
+message = f"There is a dinner at 9pm, that I would love for you to attend,{guest[2].title()}."
+print(message)
