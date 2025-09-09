f57d91a1cbb6161c58246fa56ca821cf7a892591
ronleshathomas <ronleshathomas24@gmail.com>
2025-09-09 12:11:09 -0400

    Add files via upload

---
+++ assignments/03_introducing_lists/03_03_your_own_list.py
@@ -0,0 +1,11 @@
+septa =['G', '40', '38','R' , '64']
+message = f"My first bus route was the {septa[1]}."
+print(message)
+message = f"My school bus route was the {septa[2]}."
+print(message)
+message = f"My least favorite bus route was the {septa[0]}."
+print(message)
+message = f"The dirtiest bus route was the {septa[4]}."
+print(message)
+message = f"My least traveled bus route was the {septa[3]}."
+print(message)
