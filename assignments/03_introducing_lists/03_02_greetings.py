e6d335f2e9205afd870985bc377a1888f24eddc1
ronleshathomas <ronleshathomas24@gmail.com>
2025-09-09 12:10:35 -0400

    Add files via upload

---
+++ assignments/03_introducing_lists/03_02_greetings.py
@@ -0,0 +1,7 @@
+names = ['namier', 'tamiya', 'sarai']
+message = f"My first friend was a {names[0].title()}."
+print(message)
+message = f"My first friend was a {names[1].title()}."
+print(message)
+message = f"My first friend was a {names[2].title()}."
+print(message)
