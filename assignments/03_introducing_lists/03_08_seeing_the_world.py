ce9180ed4cf072b308704fc6967b34200689fa11
ronleshathomas <ronleshathomas24@gmail.com>
2025-09-11 12:05:10 -0400

    Add files via upload

---
+++ assignments/03_introducing_lists/03_08_seeing_the_world.py
@@ -0,0 +1,13 @@
+places = ["Haiti", "Paris", "Iceland", "Jamaica", "Cairo"]
+print(places)
+print(sorted(places))
+print(places)
+print(sorted(places, reverse=True))
+places.reverse()
+print(places)
+places.reverse()
+print(places)
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
