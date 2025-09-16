6a6b8e894852669f5812c9ac4178466f3ef476f4
ronleshathomas <ronleshathomas24@gmail.com>
2025-09-16 12:20:17 -0400

    Add files via upload

---
+++ assignments/04_working_with_lists/04_10_slice.py
@@ -0,0 +1,7 @@
+animals = ['cat', 'tiger', 'panther', "dog", "wolf", "fox", "elephant", "giraffe", "bear"]
+print("the first three items on my list are")
+print(animals[0:3])
+print("three items from the middle of my list are")
+print(animals[3:6])
+print("the last three items in my list are")
+print(animals[6:10])
