8b3c77a62f61d3619745e37396d25a1bbbf85521
ronleshathomas <ronleshathomas24@gmail.com>
2025-09-16 12:20:58 -0400

    Add files via upload

---
+++ assignments/04_working_with_lists/04_11_my_pizzas_your_pizzas.py
@@ -0,0 +1,8 @@
+pizzas = ['cheese', 'alfredo', 'philly']
+friend_pizzas = ['cheese', 'alfredo', 'philly']
+pizzas.append('pepperoni')
+friend_pizzas.append('bbq chicken')
+for pizza in pizzas:
+    print(f"My favorite pizza is {pizza} pizza.")
+for pizza in friend_pizzas:
+    print(f"My friend's favorite pizza is {pizza} pizza.")
