9478863fc243f4ef93f0f034f67139356d9767d0
ronleshathomas <ronleshathomas24@gmail.com>
2025-09-16 12:41:43 -0400

    Add files via upload

---
+++ assignments/04_working_with_lists/04_13_buffet.py
@@ -0,0 +1,7 @@
+og_buffet = ("salad", "soup", "bread", "pasta", "dessert")
+for food in og_buffet:
+    print(food)
+# buffet[0] = "fruit" --- IGNORE ---
+new_buffet = ("fruit", "salad", "bread", "pasta", "dessert")
+for food in new_buffet:
+    print(food)
