801e52268f51f8af951ab3c8b05520e24ef40d43
ronleshathomas <ronleshathomas24@gmail.com>
2025-09-16 12:42:07 -0400

    Add files via upload

---
+++ assignments/04_working_with_lists/04_14_pep8.py
@@ -0,0 +1,8 @@
+"""
+From the PEP 8 page there are many things a beginner like me can take away. 
+This is the official style guide for Python code, especially code in the standard library. 
+Its purpose is to improve readability and consistency.
+Sice code is read more often than written. 
+Plus projects can have their own styles.
+Though when there’s conflict, the project-specific style usually wins.
+"""
