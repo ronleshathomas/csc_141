0285890bf4d7976ec18bf26af7123ac7d7e7d9a8
ronleshathomas <ronleshathomas24@gmail.com>
2025-09-18 12:17:55 -0400

    Add files via upload

---
+++ assignments/05_if_statements/05_02_more_conditional_tests.py
@@ -0,0 +1,42 @@
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')   
+
+print("\nIs car != 'subaru'? I predict False.")
+print(car != 'subaru')   #
+
+name = "Alice"
+print("\nIs name.lower() == 'alice'? I predict True.")
+print(name.lower() == 'alice')  
+
+print("\nIs name.lower() == 'bob'? I predict False.")
+print(name.lower() == 'bob')    
+
+age = 20
+print("\nIs age == 20? I predict True.")
+print(age == 20)   
+
+print("\nIs age < 18? I predict False.")
+print(age < 18)    
+
+num1 = 10
+num2 = 5
+print("\nIs num1 > 5 and num2 < 10? I predict True.")
+print(num1 > 5 and num2 < 10)   
+
+print("\nIs num1 < 5 or num2 > 10? I predict False.")
+print(num1 < 5 or num2 > 10)    
+
+fruits = ['apple', 'banana', 'cherry']
+print("\nIs 'apple' in fruits? I predict True.")
+print('apple' in fruits)   
+
+print("\nIs 'orange' in fruits? I predict False.")
+print('orange' in fruits)  
+
+print("\nIs 'grape' not in fruits? I predict True.")
+print('grape' not in fruits)   
+
+print("\nIs 'banana' not in fruits? I predict False.")
+print('banana' not in fruits)  
+
