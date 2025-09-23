1f399330f01ba7b0ef32c5bb7c213a9545bd5a7e
ronleshathomas <ronleshathomas24@gmail.com>
2025-09-23 11:53:43 -0400

    Add files via upload

---
+++ assignments/06_dictionaries/06_03_glossary.py
@@ -0,0 +1,10 @@
+glossary = {
+    'variable': 'A container for storing data values.',
+    'list': 'A collection of items in a particular order.',
+    'dictionary': 'A collection of key-value pairs.',
+    'loop': 'A way to repeat a block of code multiple times.',
+    'function': 'A block of code that performs a specific task.'
+}
+for word, meaning in glossary.items():
+    print(f"{word.title()}:\n  {meaning}\n")
+
