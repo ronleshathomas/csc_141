f4095fc70f613aebdf5a444890f305a702dca304
ronleshathomas <ronleshathomas24@gmail.com>
2025-09-23 11:53:00 -0400

    Add files via upload

---
+++ assignments/06_dictionaries/06_02_favorite_numbers.py
@@ -0,0 +1,12 @@
+favorite_number = {
+    'miya': '5',
+    'minah': '14',
+    'mir': '23',
+    'lee': '3',
+    'rai': '22',
+    }
+print("Miya's favorite number is " + favorite_number['miya'] + ".")
+print("Minah's favorite number is " + favorite_number['minah'] + ".")
+print("Mir's favorite number is " + favorite_number['mir'] + ".")
+print("Lee's favorite number is " + favorite_number['lee'] + ".")
+print("Rai's favorite number is " + favorite_number['rai'] + ".")
