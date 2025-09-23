cf4a173828bfd9ae3d9d8219a4afc144d1d36c8a
ronleshathomas <ronleshathomas24@gmail.com>
2025-09-23 00:04:22 -0400

    Add files via upload

---
+++ assignments/05_if_statements/05_13_your_own_ideas.py
@@ -0,0 +1,8 @@
+"""
+I could make my own ai checkers game,
+I would make a music tracker. 
+So it would track all charting songs and follow my facorite artist.
+Also a to-do list.
+That will automatically check things off as you do them. 
+"""""
+
