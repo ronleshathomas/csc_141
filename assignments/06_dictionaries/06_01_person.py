8b5d9d7f7ef75c15b350c24a6888d7be146e9404
ronleshathomas <ronleshathomas24@gmail.com>
2025-09-23 11:52:25 -0400

    Add files via upload

---
+++ assignments/06_dictionaries/06_01_person.py
@@ -0,0 +1,10 @@
+person = {
+    'first_name': 'Namier',
+    'last_name': 'Dukes',
+    'age': 20,
+    'city': 'Philadelphia'
+}
+print("First name:", person['first_name'])
+print("Last name:", person['last_name'])
+print("Age:", person['age'])
+print("City:", person['city'])
