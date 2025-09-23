f66bb68ca554eeb72923da7acf4ebfda688d5209
ronleshathomas <ronleshathomas24@gmail.com>
2025-09-22 23:53:03 -0400

    Add files via upload

---
+++ assignments/05_if_statements/05_09_no_users.py
@@ -0,0 +1,10 @@
+usernames = ["admin", "ron", "vone", "mier", "lee"]
+usernames = []
+if usernames:
+    for user in usernames:
+        if user == "admin":
+            print("Hello admin, would you like to see a status report?")
+        else:
+            print(f"Hello {user.title()}, thank you for logging in again.")
+else:
+    print("We need to find some users!")
