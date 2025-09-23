f182465381f982537706a997c187edbc5b62fd7f
ronleshathomas <ronleshathomas24@gmail.com>
2025-09-22 23:24:48 -0400

    Add files via upload

---
+++ assignments/05_if_statements/05_04_alien_colors_2.py
@@ -0,0 +1,13 @@
+alien_color = 'green'
+if alien_color == 'green':
+    print("Player just earned 5 points for shooting the alien!")
+else:
+    print("Player just earned 10 points.")
+
+alien_color = 'yellow'
+if alien_color == 'green':
+    print("Player just earned 5 points for shooting the alien!")
+else:
+    print("Player just earned 10 points.")
+
+
