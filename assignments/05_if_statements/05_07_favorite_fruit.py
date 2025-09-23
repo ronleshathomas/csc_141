936b1885bc8ff32a83ef01891c7a58ae7da01548
ronleshathomas <ronleshathomas24@gmail.com>
2025-09-22 23:37:33 -0400

    Add files via upload

---
+++ assignments/05_if_statements/05_07_favorite_fruit.py
@@ -0,0 +1,16 @@
+favorite_fruits = ["mango", "strawberry", "grape"]
+if "mango" in favorite_fruits:
+    print("You really like mangoes!")
+
+if "strawberry" in favorite_fruits:
+    print("You really like strawberries!")
+
+if "banana" in favorite_fruits:
+    print("You really like bananas!")
+
+if "apple" in favorite_fruits:
+    print("You really like apples!")
+
+if "grape" in favorite_fruits:
+    print("You really like grapes!")
+
